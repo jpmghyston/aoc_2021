1c33920622b94abb35951b47396abc4dc14c20b5
Jake Mckenna <jake.mckenna@ghyston.com>
2021-12-03 08:07:35 +0000

    Day 03 part 2

--- day03/day03.py
+++ day03/day03.py
@@ -5,8 +5,7 @@ def get_input():
         input_data = f.readlines()
     return [[int(y) for y in x.strip()] for x in input_data]
 
-def part_1():
-    vals = get_input()
+def part_1(vals):
     total_num_of_nums = len(vals)
     len_of_each_num = len(vals[0])
     bitwise_val_counts = [0 for x in range(len_of_each_num)]
@@ -16,14 +15,38 @@ def part_1():
     bitwise_gamma = [None for x in range(len_of_each_num)]
     bitwise_epsilon = [None for x in range(len_of_each_num)]
     for i in range(len_of_each_num):
-        if bitwise_val_counts[i] > total_num_of_nums/2:
+        if bitwise_val_counts[i] >= total_num_of_nums/2:
             bitwise_gamma[i] = "1"
             bitwise_epsilon[i] = "0"
         else:
             bitwise_gamma[i] = "0"
             bitwise_epsilon[i] = "1"
-    print(int("".join(bitwise_gamma), 2) * int("".join(bitwise_epsilon), 2))
+    return bitwise_gamma, bitwise_epsilon
+
+
+def part_2():
+    possible_oxygen_gen_ratings = get_input()
+    possible_co2_scrubber_ratings = get_input()
+
+    pos = 0
+    while len(possible_oxygen_gen_ratings) > 1:
+        bitwise_gamma, _ = part_1(possible_oxygen_gen_ratings)
+        possible_oxygen_gen_ratings = [x for x in possible_oxygen_gen_ratings if x[pos] == int(bitwise_gamma[pos])]
+        pos += 1
+
+    pos = 0
+    while len(possible_co2_scrubber_ratings) > 1:
+        _, bitwise_epsilon = part_1(possible_co2_scrubber_ratings)
+        possible_co2_scrubber_ratings = [x for x in possible_co2_scrubber_ratings if x[pos] == int(bitwise_epsilon[pos])]
+        pos += 1
+
+    oxygen_rating = int("".join([str(x) for x in possible_oxygen_gen_ratings[0]]), 2)
+    co2_rating = int("".join([str(x) for x in possible_co2_scrubber_ratings[0]]), 2)
+
+    print(oxygen_rating * co2_rating)
 
 
 if __name__ == '__main__':
-    part_1()
+    bitwise_gamma, bitwise_epsilon = part_1(get_input())
+    print(int("".join(bitwise_gamma), 2) * int("".join(bitwise_epsilon), 2))
+    part_2()
