919dce14f00644ec97621781efd8ab1c77faad01
Jake Mckenna <jake.mckenna@ghyston.com>
2021-12-07 08:13:39 +0000

    Refactor day 07

--- day07/day07.py
+++ day07/day07.py
@@ -3,7 +3,16 @@ def get_input():
         return [int(x) for x in f.read().split(",")]
 
 
-def part_1():
+def simple_fuel_expenditure(start, end):
+    return abs(end - start)
+
+
+def triangle_fuel_expenditure(start, end):
+    distance = abs(end - start)
+    return int((distance * (distance + 1))/2)
+
+
+def get_best_fuel(fuel_expenditure_method):
     crab_positions = get_input()
     min_position = min(crab_positions)
     max_position = max(crab_positions)
@@ -12,34 +21,22 @@ def part_1():
     for i in range(min_position, max_position + 1):
         fuel_expenditure = 0
         for position in crab_positions:
-            fuel_expenditure += abs(position - i)
+            fuel_expenditure += fuel_expenditure_method(position, i)
         if best_fuel is None:
             best_fuel = fuel_expenditure
         else:
             if fuel_expenditure < best_fuel:
                 best_fuel = fuel_expenditure
                 best_position = i
-    print(best_fuel)
+    return best_fuel
+
+
+def part_1():
+    print(get_best_fuel(simple_fuel_expenditure))
 
 
 def part_2():
-    crab_positions = get_input()
-    min_position = min(crab_positions)
-    max_position = max(crab_positions)
-    best_fuel = None
-    best_position = None
-    for i in range(min_position, max_position + 1):
-        fuel_expenditure = 0
-        for position in crab_positions:
-            distance = abs(position - i)
-            fuel_expenditure += (distance * (distance + 1))/2
-        if best_fuel is None:
-            best_fuel = fuel_expenditure
-        else:
-            if fuel_expenditure < best_fuel:
-                best_fuel = fuel_expenditure
-                best_position = i
-    print(best_fuel)
+    print(get_best_fuel(triangle_fuel_expenditure))
 
 
 if __name__ == "__main__":
